42c1377bf40bdec8502d2941fc567206923d5b4c
Amine derbal <aminedoran31@gmail.com>
2023-04-06 13:45:43 +0100

    setup electron

---
+++ public/electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: true,
+    },
+  });
+  mainWindow.loadURL(`${app.getAppPath()}\\build\\index.html`);
+  mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
